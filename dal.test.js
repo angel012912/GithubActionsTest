b5ef4f7cd91dca299f2aaa16b7aadfe994676072
Angel Garcia <69483330+a01745865@users.noreply.github.com>
2022-11-16 22:23:46 -0600

    Test02

--- dal.test.js
+++ dal.test.js
@@ -1,7 +1,22 @@
-const {users, accountInfo} = require('./dal.js');
+const supertest = require('supertest');
+const app = require('./testIndex');
 
-test('accountInfo', () => {
-    if (users != null){
-        expect(accountInfo('angel@hotmail.com')).toEqual({name: 'Angel'});
+it('Hello World', async () => {
+    try {
+        expect(1).toBe(1);
+    }catch (err){
+        console.log(err);
     }
 });
+
+it('GET /', async () => {
+    try {
+        const response = await supertest(app).get('/');
+        expect(response.status).toBe(200);
+    }catch (err){
+        console.log(err);
+    }
+});
+
+
+
