36448e5f3359d36e34a27b0065c8aef193016f54
Angel Garcia <69483330+a01745865@users.noreply.github.com>
2022-11-16 22:26:42 -0600

    Fail Test 3

--- dal.test.js
+++ dal.test.js
@@ -1,21 +1,11 @@
 const supertest = require('supertest');
 const app = require('./testIndex');
 
-it('Hello World', async () => {
-    try {
-        expect(1).toBe(1);
-    }catch (err){
-        console.log(err);
-    }
-});
-
 it('GET /', async () => {
-    try {
-        const response = await supertest(app).get('/');
-        expect(response.status).toBe(200);
-    }catch (err){
-        console.log(err);
-    }
+    
+    const response = await supertest(app).get('/');
+    console.log(response.status);
+    expect(response.status).toBe(202);
 });
 
 
